1c54c001964fcf2cfd138af159456f5b89976fa4
Laser Nite <lasernite@gmail.com>
2014-02-08 02:35:56 -0500

    fibonacci

--- week_1_homework.py
+++ week_1_homework.py
@@ -137,6 +137,56 @@ def oddx100(stuff):
         return x % 2 == 1
     return lc(f,g,stuff)
 
+def flub(stuff):
+    return lc(lambda x: x + 'ubblefub', lambda x: type(x) == str, stuff)
+
+
+# Vectors
+class V2:
+    
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def add(self, v):
+        x = self.x + v.x
+        y = self.y + v.y
+        return V2(x, y)
+
+    def mul(self, s):
+        x = self.x * s
+        y = self.y * s
+        return V2(x,y)
+
+    def __add__(self, v):
+        return self.add(v)
+
+    def __mul__(self, s):
+        return self.mul(s)
+    
+    def __str__(self):
+        return 'V2' + str([self.x, self.y])
+
+def fib(n):
+    all_fib = []
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    for x in range(n+1):
+        if x == 0:
+            all_fib.append(0)
+        elif x == 1:
+            all_fib.append(1)
+        else:
+            all_fib.append(all_fib[x-1] + all_fib[x-2])
+    return all_fib[n]
+
+
+
+
+
+
 
             
         
