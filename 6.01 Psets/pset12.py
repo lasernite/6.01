86efcb7b7bee9c04f99674a5f9ee6da81b2faf94
Laser Nite <lasernite@gmail.com>
2014-05-05 01:59:59 -0400

    pset 12 finished

--- 6.01 Psets/pset12.py
+++ 6.01 Psets/pset12.py
@@ -1,5 +1,10 @@
 import lib601.dist as dist
 from lib601.dist import *
+from lib601.hmm import *
+from math import *
+import lib601.util as util
+from lib601.util import *
+
 
 def turtlefood(initd, depth):
     h = initd[0]
@@ -76,4 +81,122 @@ def test4():
     startDist = uniformDist(range(5))
     transitionDist = lambda s: mixture(uniformDist(range(s-1,s+2)), deltaDist(s-2), 0.812699)
     ans = occupationDist(MarkovChain(startDist, transitionDist), 5).d
+    return ans
+
+kittens3 = {0:0.125, 1:0.375, 2:0.375, 3:0.125}
+kittens4 = {0:0.0625, 1:0.25, 2:0.375, 3:0.25, 4:0.0625}
+kittens5 = {0:0.03125, 1:0.15625, 2:0.3125, 3:0.3125, 4:0.15625, 5:0.03125}
+# Kitten dist with uniform dist:
+unikitten = {}
+
+for key in kittens5:
+    unikitten[key] = kittens5[key] * 1.0/3
+for key in kittens4:
+    unikitten[key] += kittens4[key] * 1.0/3
+for key in kittens3:
+    unikitten[key] += kittens3[key] * 1.0/3
+
+expected = 0
+for key in unikitten:
+    expected += unikitten[key]*key
+
+# New kitten dist expectation
+newkitten = {}
+
+for key in kittens5:
+    newkitten[key] = kittens5[key] * 0.43859649
+for key in kittens4:
+    newkitten[key] += kittens4[key] * 0.56140351
+
+newexpected = 0
+
+newexpected = 0
+for key in newkitten:
+    newexpected += newkitten[key]*key
+
+
+# State Estimator
+
+initialDist = dist.DDist({'c':0.1,'d':0.7,'e':0.2})
+def obsModel(s):
+    if s == 'c':
+        return dist.DDist({'good':0.9,'bad':0.1})
+    else:
+        return dist.DDist({'good':0.2,'bad':0.8})
+
+def nextChar(s):
+    # return next bigger character (in alpha order)
+    return chr(ord(s)+1)    
+def prevChar(s):
+    # return next smaller character (in alpha order)
+    return chr(ord(s)-1)    
+
+def transModel(s):
+    if s == 'a':
+        return dist.deltaDist('a')
+    elif s == 'z':
+        return dist.deltaDist('z')
+    else:
+        return dist.DDist({s:0.5, nextChar(s):0.25, prevChar(s):0.25})
+
+letterC = HMM(initialDist, transModel, obsModel)
+
+class StateEstimator:
+    def __init__(self, hmm):
+        self.hmm = hmm
+        self.belief = self.hmm.startDistribution
+    def transition(self):
+        transmix = dist.makeJointDistribution(self.belief, self.hmm.transitionDistribution)
+        self.belief = transmix.project(lambda (a,b): b)
+    def observe(self, obs):
+        self.belief = dist.bayesRule(self.belief, self.hmm.observationDistribution, obs)
+
+
+
+def test1a():
+    se = StateEstimator(letterC)
+    se.observe('bad')
+    se.observe('bad')
+    ans = se.belief.d
+    return ans
+
+def test2a():
+    se = StateEstimator(letterC)
+    se.transition()
+    se.transition()
+    ans = se.belief.d
+    return ans
+
+def test3a():
+    se = StateEstimator(letterC)
+    se.transition()
+    se.observe('good')
+    se.transition()
+    se.observe('bad')
+    ans = se.belief.d
+    return ans
+
+
+def sonarHit(distance, sonarPose, robotPose):
+    # Sonar hit distance from robot
+    dr = distance + sqrt(sonarPose.x**2 + sonarPose.y**2)
+    # Wall distance from robot coordinates
+    wrx = cos(robotPose.theta + sonarPose.theta) * dr
+    wry = sin(robotPose.theta + sonarPose.theta) * dr
+    print util.Pose(wrx, wry, robotPose.theta + sonarPose.theta)
+    # Global coordinates
+    gx = robotPose.x + wrx
+    gy = robotPose.y + wry
+    print util.Pose(gx, gy, robotPose.theta + sonarPose.theta)
+    # Slighty off? ~3%
+    return util.Point(gx, gy)
+
+def sonarHit(distance, sonarPose, robotPose):
+    globalsensor = robotPose.transformPose(sonarPose)
+    # Now set wall hit by continuing in direction of sonar for distance d
+    wallhit = globalsensor.transformPose(util.Pose(distance,0,0))
+    return wallhit.point()
+    
+def testsonar():
+    ans = sonarHit(0.5, util.Pose(0.15, 0, math.pi/10), util.Pose(1, 0.5, math.pi/4)).xyTuple()
     return ans
